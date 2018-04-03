77b47b9ba4cc701710174dd6f383bdeeef5040d2
Daniel <dtoro@g.clemson.edu>
2018-04-03 14:21:32 -0400

    fixed included headers

--- filter/filter.h
+++ filter/filter.h
@@ -1,8 +1,8 @@
 #ifndef FILTER_H
 #define FILTER_H
-#include "../image.Header.h"
-#include "../image.Image.h"
-#include "../image.Pixel.h"
+#include "../image/Header.h"
+#include "../image/Image.h"
+#include "../image/Pixel.h"
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>

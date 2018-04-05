dcbed3041c4464d3bf9b3ebfb9c788ae6bf194ab
Daniel <dtoro@g.clemson.edu>
2018-04-05 01:48:31 -0400

    updated kernel functions

--- filter/filter.cpp
+++ filter/filter.cpp
@@ -141,6 +141,6 @@ Image& Filter::sharpen(Image& img, Matrix& k){
 
   Image retImg(img, newPix);
   cout << "no problem with loop" << endl;
-  img = retImg;
-  return img;
+  // img = retImg;
+  return retImg&;
 }

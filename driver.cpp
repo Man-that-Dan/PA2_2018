4578ec327461d9774689125f8bd59c6009c38636
Daniel <dtoro@g.clemson.edu>
2018-04-05 01:31:26 -0400

    updated kernel functions

--- driver.cpp
+++ driver.cpp
@@ -32,10 +32,10 @@ int main(int argc, char const *argv[]) {
   // ifstream constructor, let's read the file twice!
   Image pic(input);
   Image outK5 = Filter::sharpen(pic, Filter::K5);
-  // Image outK3 = Filter::sharpen(pic, Filter::K3);
+  Image outK3 = Filter::sharpen(pic, Filter::K3);
 
   outK5.write_to(output2);
-  // outK3.write_to(output);
+  outK3.write_to(output);
 
 
   input.close();
